025b86e1969abeeba67e05bd457ac10771a3dfb4
Dan Burrows <dannyburrows@protonmail.com>
2020-10-26 14:09:05 +0000

    Removing hardcoded secret key, updating gitignore and requirements

--- projectCode/__init__.py
+++ projectCode/__init__.py
@@ -2,14 +2,17 @@
 # builds the app and initializes the server.
 
 import os
+import secrets
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
 
+secret_key = secrets.token_hex(16)
+
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+app.config['SECRET_KEY'] = secret_key
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
